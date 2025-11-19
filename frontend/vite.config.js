1b7569979e2c9bc7feaf99e4f5e3985953d7301a
bryan lee <bryan900926@gmail.com>
2025-11-19 15:35:54 +0800

    setting configure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,18 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  server: {
+    host: "0.0.0.0",
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    proxy: {
+      "/api": "http://backend:5000", //since we are in docker network, use service name 'backend' and port 5000
+    },
+  },
+});
